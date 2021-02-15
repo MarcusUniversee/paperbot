d3ee6711deaaf39dc5aac390e7eaff4487f21073
Stan-21 <77824280+Stan-21@users.noreply.github.com>
2021-02-14 18:36:04 -0800

    Created horse
    I created horse and this is also kinda a test to see if everything works

---
+++ commands/horse.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js')
+
+var paperFacts = ['COMING SOON'
+                 ]
+
+module.exports = {
+  name: 'horse',
+  description: 'Replies with a horse fact',
+  expectedArgs: '',
+  category: 'Facts',
+  permissionError: '',
+  minArgs: 0,
+  maxArgs: 1,
+  callback: async (message, paramsCom) => {
+    console.log(message.author.tag + ' horse');
+    var paperNum = await Math.floor((Math.random() * paperFacts.length));
+    message.reply(paperFacts[paperNum]);
+  },
+  permissions: [],
+  requiredRoles: [],
+}
